f5ad91e16b0c3f5a6e317b23997e1a7bd66f0088
computer0101 <deveshpatel7643@gmail.com>
2018-06-14 13:29:23 +0530

    updated README

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="title" color="inherit">
-          <Link to="/" className='my-books'>My Books</Link>
+          <Link to="/" className='my-books'>My Reads</Link>
         </Typography>
         <Link to='/search' className="search-button"><Button color="inherit">Search</Button></Link>
       </Toolbar>
